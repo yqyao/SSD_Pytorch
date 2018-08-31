9161087ca85426ca2cd1f952cd8caa4610d876ba
yqyao <1073079979@qq.com>
2018-08-31 11:43:49 +0800

    fix bugs

--- layers/functions/__init__.py
+++ layers/functions/__init__.py
@@ -1,6 +1,6 @@
 from .detection import Detect
 from .prior_box import PriorBox
-from .refine_prior_box import RefinePriorBox
+# from .refine_prior_box import RefinePriorBox
 
 
 __all__ = ['Detect', 'PriorBox']
